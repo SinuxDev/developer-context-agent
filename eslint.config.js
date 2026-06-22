822336e17de532f8c096b6589f8b0eb2e2e30da4
Aung Ye Htet <aung.atg1738@gmail.com>
2026-06-22 23:00:08 +0630

    chore(submoudle): initialize typescript project and tooling

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+import eslint from '@eslint/js';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsparser from '@typescript-eslint/parser';
+
+export default [
+  eslint.configs.recommended,
+  {
+    files: ['src/**/*.ts', 'tests/**/*.ts'],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: { project: './tsconfig.test.json' },
+    },
+    plugins: { '@typescript-eslint': tseslint },
+    rules: {
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    },
+  },
+];
